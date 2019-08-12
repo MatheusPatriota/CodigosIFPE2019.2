1aad87de86bef4a626c7971903ec6a706c0af68c
MatheusPatriota <matheus.patriota@ccc.ufcg.edu.br>
2019-08-12 10:58:01 -0300

    desafios terminados

--- AGOSTO2019/alvos.py
+++ AGOSTO2019/alvos.py
@@ -1,10 +1,28 @@
+import math
+
 #  5000 alvos
 #  6 circulos
 #  diamentro do circulo maior sera dado pelo user
-
-
 # convencionado em CM
-diametroMaior = input("digite o diametro do maior circulo: ")
 
-raio = int(diametroMaior) / 2
-percentualDeDiminuicao = raio / 6
+
+def calculoAreaTotal(diametroMaior, percentual, saida):
+    if diametroMaior <= percentual:
+        return 0
+    saida = calculoAreaCirculo(
+        diametroMaior) + calculoAreaTotal(diametroMaior-percentual, percentual, saida)
+    return saida
+
+
+def calculoAreaCirculo(diametro):
+    raio = diametro/2
+    area = (math.pi * (raio**2))
+    return area
+
+
+diametroMaior = input("digite o diametro do maior circulo: ")
+s = 0
+percentualDeDiminuicao = float(diametroMaior) / 6
+saida = calculoAreaTotal(float(diametroMaior), percentualDeDiminuicao, s)
+print(
+    "\no total de papelao a ser utilizado serah de {0:0.0f} cm²" .format(saida*5000))
--- AGOSTO2019/ponte.py
+++ AGOSTO2019/ponte.py
@@ -1,6 +1,8 @@
+import math
+
 # o comprimento da ponte eh fornecido em KM entre 2 - 4
 # tera 4 hastes de sustentacao
-# 1/20 da extensao da ponte 
+# 1/20 da extensao da ponte
 # 5 cabos de aco por haste
 
 
@@ -9,22 +11,29 @@ def calculaQuantidadeCabosMetros(c):
     temp = c
     saida = 0
     while temp > 0:
-        saida += temp 
+        saida += temp
         temp -= equi
-    saida = saida *4
-    print ("\nO total de metros de cabo de aco necessario eh {0:0.0f}".format(saida) )
+    saida = saida * 4
+    print(
+        "\nO total de metros de cabo de aco necessario eh {0:0.0f}".format(saida))
+
+
+def calculoHipotenusa(cat1, cat2):
+    temp = (cat1**2) + (cat2 ** 2)
+    hip = math.sqrt(temp)
+    return hip
 
 
 # trabalhar com conversoes em metros
-while True:  
-    extensao = input("Qual serah a extensao da ponte ? (Deve ser entre (2 - 4) KM): ")
-    if(int(extensao) >=2 and int(extensao) <=4):  
-        break  
+while True:
+    extensao = input(
+        "Qual serah a extensao da ponte ? (Deve ser entre (2 - 4) KM): ")
+    if(float(extensao) >= 2 and float(extensao) <= 4):
+        break
 
 
-alturaHaste = (int(extensao) * (1/20)) * 1000
-comprimentoMaiorCabo = (int(extensao) / 2 ) * 1000
+alturaHaste = (float(extensao) * (1/20)) * 1000
+comprimentoMaiorCabo = calculoHipotenusa(alturaHaste, (float(extensao)/2)*1000)
 # 1000/5 = 200
 
-calculaQuantidadeCabosMetros (comprimentoMaiorCabo)
-
+calculaQuantidadeCabosMetros(comprimentoMaiorCabo)
